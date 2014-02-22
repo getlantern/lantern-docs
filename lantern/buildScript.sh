b33130281948d2f825393405ac1f3d75064b9a6c
tlevinson <Todd@todds-air.westell.com>
2014-02-22 13:46:00 -0500

    adding rst docs

---
+++ lantern/buildScript.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Delete the build directory and recreate it to make sure sphinx-doc pulls in any updates to template files
+
+DIR="_build"
+
+#Check that it exists before trying to delete
+
+if [ -d "$DIR" ]
+then
+	rm -f -r $DIR
+fi
+
+#Add back the _build dir
+mkdir _build
+
+find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir sudo mkdir -p _build/langDir
+chmod a+rw  _build
+
+#Find all the directories that have an index.rst file and create a new subfolder under _build
+#for each language and category (user or developer) of the docs. 
+
+find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir sudo mkdir -p _build/langDir
+
+chmod -R a+rw _build
+
+#Next run the build script for every directory that has and index.rst file and output the results to the 
+#correct folder under build
+
+find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir sphinx-build -b html langDir _build/langDir
+
+
