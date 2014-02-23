d160e9f6f36a76436b3bdf34c0acd986d8b39f5d
levinsontodd@gmail.com <levinsontodd@gmail.com>
2014-02-23 08:15:53 -0500

    Changed output directory to html instead of _build and replaced build directory path with a variable.

--- lantern/buildScript.sh
+++ lantern/buildScript.sh
@@ -2,7 +2,7 @@
 
 #Delete the build directory and recreate it to make sure sphinx-doc pulls in any updates to template files
 
-DIR="_build"
+DIR="html"
 
 #Check that it exists before trying to delete
 
@@ -12,18 +12,18 @@ then
 fi
 
 #Add back the _build dir
-mkdir _build
+mkdir $DIR
 
-chmod a+rw  _build
+chmod a+rw $DIR
 
-#Find all the directories that have an index.rst file and create a new subfolder under _build
-find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir mkdir -p _build/langDir
+#Find all the directories that have an index.rst file and create a new subfolder under DIR
+find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir mkdir -p $DIR/langDir
 
-chmod -R a+rw _build
+chmod -R a+rw $DIR
 
 #Next run the build script for every directory that has and index.rst file and output the results to the 
 #correct folder under build
 
-find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir sphinx-build -b html langDir _build/langDir
+find . -name index.rst | xargs -I fPath dirname fPath | sed 's#\./##g' | xargs -I langDir sphinx-build -b html langDir $DIR/langDir
 
 
